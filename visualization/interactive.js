63987a09ec8898c26d5a9b21ea972877b92d5293
Zach Collins <collinzy@mail.uc.edu>
2017-10-28 21:14:44 -0400

    split up pages

---
+++ visualization/interactive.js
@@ -0,0 +1,74 @@
+var arr = [];
+
+function parseCSV(str) {
+    var quote = false;  // true means we're inside a quoted field
+
+    // iterate over each character, keep track of current row and column (of the returned array)
+    for (var row = col = c = 0; c < str.length; c++) {
+        var cc = str[c], nc = str[c+1];        // current character, next character
+        arr[row] = arr[row] || [];             // create a new row if necessary
+        arr[row][col] = arr[row][col] || '';   // create a new column (start with empty string) if necessary
+
+        // If the current character is a quotation mark, and we're inside a
+        // quoted field, and the next character is also a quotation mark,
+        // add a quotation mark to the current column and skip the next character
+        if (cc == '"' && quote && nc == '"') { arr[row][col] += cc; ++c; continue; }  
+
+        // If it's just one quotation mark, begin/end quoted field
+        if (cc == '"') { quote = !quote; continue; }
+
+        // If it's a comma and we're not in a quoted field, move on to the next column
+        if (cc == ',' && !quote) { ++col; continue; }
+
+        // If it's a newline (CRLF) and we're not in a quoted field, skip the next character
+        // and move on to the next row and move to column 0 of that new row
+        if (cc == '\r' && nc == '\n' && !quote) { ++row; col = 0; ++c; continue; }
+
+        // If it's a newline (LF or CR) and we're not in a quoted field,
+        // move on to the next row and move to column 0 of that new row
+        if (cc == '\n' && !quote) { ++row; col = 0; continue; }
+        if (cc == '\r' && !quote) { ++row; col = 0; continue; }
+
+        // Otherwise, append the current character to the current column
+        arr[row][col] += cc;
+    }
+    arr.shift();
+    firsts = [];
+    for (i = 0; i < arr.length; i++) {
+        var parts = arr[i][1].split('-');
+        date = new Date('20' + parts[2], parts[1] - 1, parts[0]);
+        firsts.push(date.toISOString());
+    }
+    return firsts;
+}
+
+var x = parseCSV(text);
+var trace = {
+    x: x,
+    type: 'histogram',
+    nbinsx: 365
+};
+var layout = {
+    title: 'Crimes of Selected Types Per Day',
+    xaxis: {
+        title: 'Date',
+        titlefont: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+        }
+    },
+    yaxis: {
+        title: 'Crime Count',
+        titlefont: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+        }
+    }
+}
+var data = [trace];
+
+Plotly.newPlot('myDiv', data, layout);
+
+
